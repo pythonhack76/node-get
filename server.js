748f62012b5d3cb698f37ad4de05e0bd073b76b1
Python The Hack <lrulvoni@gmail.com>
2022-06-03 13:28:17 +0200

    update app.use

--- server.js
+++ server.js
@@ -1,9 +1,34 @@
 
+const { tr } = require('date-fns/locale');
 const express = require('express');
 const app = express(); 
+const { logger , timing } = require('./middleware/logEvents');
 const path = require('path');
 
+
 const PORT = process.env.PORT || 3501;
+
+//custom middleware logger
+app.use(logger); 
+app.use(timing);
+
+// app.use((req, res, next) => {
+//     logEvents(`${req.method}\t${req.headers.origin}\t${req.url}`, 'reqLogNew.txt');
+
+//     console.log(`${req.method} ${req.path}`);
+//     next(); 
+// }); 
+
+//middleware urlencoded
+app.use(express.urlencoded({ extended: false }));
+
+//json middleware
+app.use(express.json()); 
+
+//serve static files
+app.use(express.static(path.join(__dirname, '/public')));
+
+
 
 app.get('^/$|/index(.html)?', (req, res) => {
 
@@ -35,5 +60,41 @@ app.get('/old-page(.html)?', (req, res) => {
     res.redirect('/new-page.html');//302 default
 
 });
+
+
+const one = (req, res, next) => {
+    console.log('one');
+    next();
+}
+
+const two = (req, res, next) => {
+    console.log('two');
+    next(); 
+}
+
+const three = (req, res) => {
+    console.log('three');
+    res.send('piuaciuto che è già finito...?!');
+   
+}
+
+app.get('/chain(.html)?', [one, two, three]);
+
+
+//routes gestione 
+app.get('/test(.html)?', (req, res, next) => {
+    console.log('in attesa del caricmaneto del file...');
+    next() 
+}, (req, res) => {
+        res.send('ciao a tutti');
+})
+
+//gestione della 404 
+app.get('/*', (req, res) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+let bad_words = ['cazzo','minchia','merda','fica','troia'];
+
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
